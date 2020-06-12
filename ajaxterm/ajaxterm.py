3999458923a12b76931680037c8b8991c6a9c8d3
Samuel Caldas <samuel.caldas@gmail.com>
2020-06-12 04:25:03 +0000

    fix string formating

--- ajaxterm/ajaxterm.py
+++ ajaxterm/ajaxterm.py
@@ -549,16 +549,16 @@ def main():
 				file(o.pidfile,'w+').write(str(pid)+'\n')
 			except:
 				pass
-			print 'AjaxTerm at http://localhost:%s/ pid: %d' % (o.port,pid)
+			print ('AjaxTerm at http://localhost:{0}/ pid: {1}'.format(o.port,pid))
 			sys.exit(0)
 	else:
-		print 'AjaxTerm at http://localhost:%s/' % o.port
+		print ('AjaxTerm at http://localhost:{0}/'.format(o.port))
 	at=AjaxTerm(o.cmd,o.index_file)
 #	f=lambda:os.system('firefox http://localhost:%s/&'%o.port)
 #	qweb.qweb_wsgi_autorun(at,ip='localhost',port=int(o.port),threaded=0,log=o.log,callback_ready=None)
 	try:
 		qweb.QWebWSGIServer(at,ip='localhost',port=int(o.port),threaded=0,log=o.log).serve_forever()
-	except KeyboardInterrupt,e:
+	except KeyboardInterrupt as e:
 		sys.excepthook(*sys.exc_info())
 	at.multi.die()
 
